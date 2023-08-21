7001c33eccb0733389e4c34eb0ad23104004f2b5
zhufeip <141732041+zhufeip@users.noreply.github.com>
2023-08-21 10:52:20 +0800

    Update rand number.c
    Fix some problem.

--- rand number.c
+++ rand number.c
@@ -1,16 +1,18 @@
+#include<stdlib.h>
+#include<time.h>
 #include<stdio.h>
-int FuncRand(int range)
-//随机数函数，能够调整范围[0,range-1]
+int FuncRand(int range1,int range2)
+//随机数函数，能够调整范围[range1,range2]
 {
 	int Num;
 	srand((unsigned)time(NULL));
-	Num=rand() % (range);
-	return Num;
+	Num=rand() % (range2-range1);
+	return Num+(range1);
 }
 //运用
 int mian()
 {
-  int randnum=FuncRand(101);//生成一个[0-100]的随机数
+  int randnum=FuncRand(0,101);//生成一个[0-100]的随机数
   printf("%d",randnum);
   return 0;
 }
